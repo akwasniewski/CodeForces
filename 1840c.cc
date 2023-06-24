899a35647a5bf4e8f6c16894090d6da2da46631d
akwasniewski <akwasniewski.pl@gmail.com>
2023-06-24 14:16:20 +0200

    ski resort

---
+++ 1840c.cc
@@ -0,0 +1,27 @@
+//ski resort
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int s;
+    cin>>s;
+    for(;s>0;s--){
+        int dayCount, minDays, maxTemp;
+        cin>>dayCount>>minDays>>maxTemp;
+        long long possibilities=0;
+        int daysInRow=0;
+        for(;dayCount>0;dayCount--){
+            int temperature;
+            cin>>temperature;
+            if(temperature>maxTemp){
+                daysInRow=0;
+                continue;
+            }
+            daysInRow++;
+            long long curPossibilities=daysInRow-minDays+1;
+            if(curPossibilities>0) possibilities+=curPossibilities;
+        }
+        cout<<possibilities<<"\n";
+    }
+}
