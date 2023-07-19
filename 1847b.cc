8095dcb0c03b9fba221bab5ec7bf876cd331b7ff
akwasniewski <akwasniewski.pl@gmail.com>
2023-07-19 23:35:38 +0200

    Problem 1847b

---
+++ 1847b.cc
@@ -0,0 +1,34 @@
+//Hamon Oddysey
+//https://codeforces.com/contest/1847/problem/B
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int s;
+    cin>>s;
+    for(;s>0;s--){
+        int vampireCount;
+        cin>>vampireCount;
+        unsigned int vampires[vampireCount];
+        unsigned int weakestLink=UINT32_MAX;
+        for(int i=0; i<vampireCount; i++){
+            cin>>vampires[i];
+            weakestLink&=vampires[i];
+        }
+        if(weakestLink!=0){
+            cout<<1<<"\n";
+            continue;
+        }
+        int sum=0;
+        unsigned int curSubArray=0;
+        for(int i=0; i<vampireCount; i++){
+            if(curSubArray==0) curSubArray=vampires[i];
+            else curSubArray&=vampires[i];
+            if(curSubArray==0){
+                sum++;
+            }
+        }
+        cout<<sum<<"\n";
+    }
+}
