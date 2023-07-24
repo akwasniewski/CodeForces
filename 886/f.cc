b6eed36c96e4b5311ea851327c4b6597eb04fbaa
akwasniewski <akwasniewski.pl@gmail.com>
2023-07-24 22:20:15 +0200

    We were both children

---
+++ 886/f.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <utility>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int s;
+    cin>>s;
+    for(;s>0;s--){
+        int frogCount;
+        cin>>frogCount;
+        unordered_map<int, int> frogHops;
+        for(int i=0; i<frogCount; i++){
+            int hop;
+            cin>>hop;
+            if(hop<=frogCount){
+                frogHops.emplace(hop, 0);
+                frogHops[hop]++;
+            }
+        }
+
+        int catchCount[frogCount];
+        for(int i=0; i<frogCount; i++) catchCount[i]=0;
+        for(pair<int,int> curHop: frogHops){
+            int curPos=curHop.first;
+            while(curPos<=frogCount){
+                catchCount[curPos-1]+=curHop.second;
+                curPos+=curHop.first;
+            }
+        }
+        cout<<*max_element(catchCount, catchCount+frogCount)<<"\n";
+    }
+}
